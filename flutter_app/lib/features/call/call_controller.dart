ff183bb0e54f2883dbfce2423fc4d05fb8b1a743
Jamshiddin Babajonov <jbm050690hotmail.com@Jamshiddins-MacBook-Pro.local>
2026-05-18 23:09:38 +0500

    Calling is setiings

--- flutter_app/lib/features/call/call_controller.dart
+++ flutter_app/lib/features/call/call_controller.dart
@@ -680,6 +680,7 @@ class CallController extends ChangeNotifier {
 
     pc.onIceCandidate = (candidate) {
       if (candidate.candidate == null || _targetUsername == null) return;
+      if (!candidate.candidate!.contains('typ relay')) return;
       _socketService.emit('ICE_CANDIDATE', {
         'callId': _callId,
         'target': _targetUsername,
