5fafe6d244732d6f4c5e71e3b4f67148e85477bd
Jamshiddin Babajonov <jbm050690@hotmail.com>
2026-06-24 15:36:39 +0500

    Programga kirmasdan qo'ng'iroqlarga javob berish qismi sozlandi!

--- backend/src/config/fcm.js
+++ backend/src/config/fcm.js
@@ -31,13 +31,6 @@ export async function sendCallFcm(token, { callerName, isVideo, callId }) {
       android: {
         priority: 'high',
         ttl: 60000,
-        notification: {
-          title: callerName,
-          body: isVideo ? "Video qo'ng'iroq" : "Ovozli qo'ng'iroq",
-          channelId: 'incoming_calls',
-          sound: 'default',
-          color: '#4D82E3',
-        },
       },
       data: {
         type: 'incoming_call',
