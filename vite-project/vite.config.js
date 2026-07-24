0272521557e4b51b6e05266d561d013911182161
Jamshiddin Babajonov <jbm050690@hotmail.com>
2026-07-24 19:25:36 +0500

    fix: qo'ng'iroq tugagach lastActive yangilanadi

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    base: env.VITE_BASE_PATH || "/",
+    base: "./", // <--- Mana bu yerni shunday o'zgartiring
 
     server: {
       host: true,
